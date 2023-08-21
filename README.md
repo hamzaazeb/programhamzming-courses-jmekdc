881d986f14848f448f12031dc7fc930aca01e82a
hamzaazeb <121358672+hamzaazeb@users.noreply.github.com>
2023-08-21 10:09:44 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programhamzming-courses-jmekdc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-jmekdc)
---
+++ index.ts
@@ -0,0 +1,22 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+
+let azeb1: number;
+let azeb2: number;
+let azeb3: number;
+let azeb4: number;
+let azeb5: number;
+
+azeb1 = 30;
+azeb2 = 40;
+azeb3 = 50;
+azeb4 = 10;
+
+azeb5 = azeb1 * azeb2 * azeb3 * azeb4;
+
+console.log(azeb1);
+
+console.log(azeb2);
+console.log(azeb3);
